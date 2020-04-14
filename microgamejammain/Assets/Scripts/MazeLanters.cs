76354b684b3ffc974af8d7d65cf55adbbc9e9181
ComradeHappyPug <esther.strathy@digipen.edu>
2020-04-14 10:16:25 -0700

    folders and scripts updates

---
+++ microgamejammain/Assets/Scripts/MazeLanters.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeLanters : MonoBehaviour
+{
+    public Sprite[] lanterns;
+    SpriteRenderer spriterend;
+    int x; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriterend = GetComponent<SpriteRenderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator randomlantern()
+    {
+        x = Random.Range(0, 3);
+        spriterend.sprite = lanterns[x];
+        yield return new WaitForSecondsRealtime(1f);
+    }
+
+}
---
+++ microgamejammain/Assets/Scripts/mazemovement.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * mazemovement.cs
+ * By: Esther Strathy
+ * 4/14/2020
+ * Description: allows for character movement in the maze minigame 
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mazemovement : MonoBehaviour
+{
+    Rigidbody2D myrb;
+    [SerializeField] float acceleration = 1.2f;
+    [SerializeField] float deceleration = .5f;
+    [SerializeField] float speed = 5f;
+
+    Vector2 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myrb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+
+    void move()
+    {
+        //horizontal movement
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+                velocity.x = Mathf.MoveTowards(velocity.x, speed * Input.GetAxis("Horizontal"), acceleration * Time.fixedDeltaTime);
+                myrb.velocity = new Vector2(velocity.x, velocity.y);
+        }
+        else
+        {
+
+            velocity.x = Mathf.MoveTowards(velocity.x, speed * Input.GetAxis("Horizontal"), deceleration * Time.fixedDeltaTime);
+            myrb.velocity = new Vector2(velocity.x, velocity.y);
+        }
+        
+        //vertical movment
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            velocity.y = Mathf.MoveTowards(velocity.y, speed * Input.GetAxis("Vertical"), acceleration * Time.fixedDeltaTime);
+            myrb.velocity = new Vector2(velocity.x, velocity.y);
+        }
+        else
+        {
+
+            velocity.y = Mathf.MoveTowards(velocity.y, speed * Input.GetAxis("Vertical"), deceleration * Time.fixedDeltaTime);
+            myrb.velocity = new Vector2(velocity.x, velocity.y);
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
