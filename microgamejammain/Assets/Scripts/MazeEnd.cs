b1cac3511f445648bff023517ca10ae102d04395
ComradeHappyPug <esther.strathy@digipen.edu>
2020-04-15 09:56:01 -0700

    my work for today

---
+++ microgamejammain/Assets/Scripts/MazeEnd.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * MazeEnd.cs
+ * By: Esther Strathy
+ * 4/15/2020
+ * Description: win conditions of maze minigame
+ */
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeEnd : MonoBehaviour
+{
+    int lanterns;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if at end of stage and lanterns still are on the level lose a life
+        if (lanterncount() != 0)
+        {
+            GameManager.lives -= 1;
+        }
+    }
+
+    int lanterncount()
+    {
+        lanterns = GameObject.FindObjectsOfType<MazeLanters>().Length;
+        return lanterns;
+    }
+}
--- microgamejammain/Assets/Scripts/gamemanager.cs
+++ microgamejammain/Assets/Scripts/gamemanager.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static int lives = 5; 
+    public static int lives = 5;
+    [SerializeField] static int timer; 
     // Start is called before the first frame update
     void Start()
     {
