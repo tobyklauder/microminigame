c86103171ba01a211df359104cd7128cbc0d2ec8
tobyklauder <tobyklauder@hotmail.com>
2020-04-15 09:17:53 -0700

    switch scene every 5 seconds

--- microgamejammain/Assets/Scripts/pickgame.cs
+++ microgamejammain/Assets/Scripts/pickgame.cs
@@ -7,23 +7,13 @@ public class pickgame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SceneManager.LoadScene(Random.Range(0,9));
+        // TimerCoroutine();
+        Invoke("delayedscenechange", 5f);
     }
-    IEnumerator TimerCoroutine()
-    {
-        //Print the time of when the function is first called.
-        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+  
+ 
 
-        //yield on a new YieldInstruction that waits for 5 seconds.
-        yield return new WaitForSeconds(5);
-
-        //After we have waited 5 seconds print the time again.
-        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        TimerCoroutine();
+    void delayedscenechange() {
         SceneManager.LoadScene(Random.Range(0, 9));
     }
 }
