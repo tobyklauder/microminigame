e836d00024798a0ac530a1f523be60b57172157e
tobyklauder <tobyklauder@hotmail.com>
2020-04-15 09:08:38 -0700

    add pickgame script - pending test

---
+++ microgamejammain/Assets/Scripts/pickgame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+public class pickgame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene(Random.Range(0,9));
+    }
+    IEnumerator TimerCoroutine()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(5);
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        TimerCoroutine();
+        SceneManager.LoadScene(Random.Range(0, 9));
+    }
+}
