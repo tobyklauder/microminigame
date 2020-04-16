7722ce588995893c82208e65c798bdc5a054ab77
Raine-Johnson <56552386+Raine-Johnson@users.noreply.github.com>
2020-04-16 09:42:50 -0700

    Raine Fly Game
    Added my fly game, almost done just need background and sound

---
+++ microgamejammain/Assets/Scripts/raineScripts/SwatFlies.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwatFlies : MonoBehaviour
+{
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+}
