6bcb8687c1d8d18d00b33102151d731af25afd98
Younget <48969941+Younget@users.noreply.github.com>
2020-04-15 22:41:20 -0700

    Final Changes for the night
    -Ethan

--- microgamejammain/Assets/Scripts/ethansScripts/ethanPlayerController.cs
+++ microgamejammain/Assets/Scripts/ethansScripts/ethanPlayerController.cs
@@ -8,31 +8,46 @@ public class ethanPlayerController : MonoBehaviour
     public Rigidbody2D rb;
     public float speed = 3f;
 
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+            animator.SetBool("pinMoving", true);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+            animator.SetBool("pinMoving", true);
         }
-        if (Input.GetKey(KeyCode.W))
+        else if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector2.up * speed * Time.deltaTime);
+
+            animator.SetBool("pinMoving", true);
         }
-        if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+            animator.SetBool("pinMoving", true);
+        }
+
+        else
+        {
+            animator.SetBool("pinMoving", false);
         }
     }
 }
