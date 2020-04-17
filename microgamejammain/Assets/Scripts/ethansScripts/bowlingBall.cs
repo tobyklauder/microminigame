ea303cca079c2efdd1045a9c1ee47318f62fa64a
Younget <48969941+Younget@users.noreply.github.com>
2020-04-17 08:57:05 -0700

    Fixed Script and added script
    -Ethan

--- microgamejammain/Assets/Scripts/ethansScripts/bowlingBall.cs
+++ microgamejammain/Assets/Scripts/ethansScripts/bowlingBall.cs
@@ -4,22 +4,18 @@ using UnityEngine;
 
 public class bowlingBall : MonoBehaviour
 {
-    /*public Transform spawnPoint;
+    
 
-    public GameObject ballPrefab;*/
-
-    public float speed = 10f;
-    public Rigidbody2D rb;
-    int randz; 
+    public GameObject ballPrefab;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        randz = Random.Range(45, 135);
+        Vector3 position = new Vector3(Random.Range(-6.0f, 6.0f), -4);
 
-        /*Instantiate(ballPrefab, spawnPoint.position, spawnPoint.rotation);*/
-        transform.Rotate(0, 0, randz);
-        rb.velocity = transform.up * speed;
+        Instantiate(ballPrefab, position, Quaternion.Euler(0.0f, 0.0f, Random.Range(45.0f, 135.0f)));
+        /*transform.Rotate(0, 0, randz);*/
     }
 
     // Update is called once per frame
