f9d5b4913e811d5f8eacea42f726e8417457f636
Younget <48969941+Younget@users.noreply.github.com>
2020-04-17 08:13:05 -0700

    Script and Ball
    -Ethan

---
+++ microgamejammain/Assets/Scripts/ethansScripts/bowlingBall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bowlingBall : MonoBehaviour
+{
+    /*public Transform spawnPoint;
+
+    public GameObject ballPrefab;*/
+
+    public float speed = 10f;
+    public Rigidbody2D rb;
+    int randz; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randz = Random.Range(45, 135);
+
+        /*Instantiate(ballPrefab, spawnPoint.position, spawnPoint.rotation);*/
+        transform.Rotate(0, 0, randz);
+        rb.velocity = transform.up * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
